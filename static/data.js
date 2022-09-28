a6f71a93a04250ccfc8ec835a3550fb2694605da
Mustafa GÜNEY <mg@tipiteq.com>
2022-09-29 00:55:25 +0300

    Icons data list has been created

---
+++ static/data.js
@@ -0,0 +1,115 @@
+export const tools = [
+  {
+    svg: 'figma',
+    title: 'Figma',
+  },
+  {
+    svg: 'xd',
+    title: 'Adobe XD',
+  },
+  {
+    svg: 'photoshop',
+    title: 'Photoshop',
+  },
+  {
+    svg: 'vscode',
+    title: 'VS Code',
+  },
+  {
+    svg: 'bitbucket',
+    title: 'BitBucket',
+  },
+  {
+    svg: 'jira',
+    title: 'Jira',
+  },
+  {
+    svg: 'strapi',
+    title: 'Strapi',
+  },
+  {
+    svg: 'docker',
+    title: 'Docker',
+  },
+  {
+    svg: 'git',
+    title: 'Git',
+  },
+]
+
+export const langs = [
+  {
+    svg: 'html',
+    title: 'HTML',
+  },
+  {
+    svg: 'css',
+    title: 'CSS',
+  },
+  {
+    svg: 'sass',
+    title: 'SASS',
+  },
+  {
+    svg: 'javascript',
+    title: 'Javascript',
+  },
+  {
+    svg: 'typescript',
+    title: 'Typescript',
+  },
+  {
+    svg: 'vuejs',
+    title: 'Vue.js',
+  },
+  {
+    svg: 'nuxtjs',
+    title: 'Nuxt.js',
+  },
+  {
+    svg: 'vuetify',
+    title: 'Vuetify',
+  },
+  {
+    svg: 'bootstrap',
+    title: 'Bootstrap',
+  },
+  {
+    svg: 'tailwindcss',
+    title: 'Tailwindcss',
+  }
+]
+
+export const social = [
+  {
+    title: "Dribble",
+    svg: "dribble",
+    url: "https://dribbble.com/mustafaguney"
+  },
+  {
+    title: "Behance",
+    svg: "behance",
+    url: "https://www.behance.net/mustafagney"
+  },
+  {
+    title: "Instagram",
+    svg: "instagram",
+    url: "https://www.instagram.com/mmustafaguneyy"
+  },
+  {
+    title: "Linkedin",
+    svg: "linkedin",
+    url: "https://www.linkedin.com/in/mustafa-g%C3%BCney-57858220a/"
+  },
+  {
+    title: "GitHub",
+    svg: "github",
+    url: "https://github.com/muguney"
+  },
+  {
+    title: "E-Mail",
+    svg: "email",
+    url: "mailto:mail@mustafaguney.com"
+  },
+
+]
