3bc61e2eec22c45eab7a65ea937f3237ef7ef937
Mustafa GÜNEY <mg@tipiteq.com>
2022-10-02 02:00:46 +0300

    Social / Icon buttons component created, Profil section created

--- components/iconButton.vue
+++ components/iconButton.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="flex justify-center gap-4">
-    <a v-for="item in buttons" :key="item.svg" class="flex justify-start ">
-      <img  class="fill-dark" :src="`icons/${item.svg}.svg`" />
+    <a
+      v-for="item in buttons"
+      :key="item.svg"
+      class="
+        flex
+        justify-start
+        bg-white
+        text-dark
+        px-3
+        py-1
+        rounded-md
+        drop-shadow-sm
+      "
+    >
+      <img class="fill-dark mr-2" :src="`icons/${item.svg}.svg`" />
       <span :class="text ? 'text-base bg-red' : null">{{
         text ? item.title : null
       }}</span>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
-    <div class="flex flex-col justify-center items-center gap-2">
-      <h1>My Personal Website</h1>
+    <div class="flex flex-col justify-center items-center">
+      <avatar
+        :title="profile.name"
+        :image="profile.avatarImg"
+        width="150"
+        height="150"
+      />
+      <h1 class="text-xl font-semibold">{{ profile.name }}</h1>
+      <h2>{{ profile.profileTitle }}</h2>
+      <p class="mt-3 max-w-[1200px] text-center leading-relaxed">
+        {{ profile.aboutMe }}
+      </p>
+      <social-buttons class="mt-5 pt-5 border-t-[1px]" />
+      <h3 class="mt-[65px] font-semibold text-lg">Tools & Langs</h3>
+      <icon-button :buttons="tools" class="mb-4 mt-5" :text="true" />
+      <icon-button :buttons="langs" class="mb-5" :text="true" />
+      <h3 class="mt-40 text-xl font-semibold">Projects</h3>
+
+      <div class="media-gallery grid sd:grid-cols-2 lg:grid-cols-3 gap-3">
+
+
+
+      </div>
+
       <div class="flex flex-row gap-5 mb-3">
         <div
           @click="$colorMode.preference = 'dark'"
@@ -33,25 +55,24 @@
         </div>
       </div>
     </div>
-    <icon-button :buttons="tools" :text="true" />
-    <icon-button :buttons="langs" :text="true" />
-    <social-buttons />
   </div>
 </template>
 
 <script>
-import { tools, langs, social } from 'static/data'
+import { tools, langs, profile } from 'static/data'
 import iconButton from '~/components/iconButton.vue'
+import avatar from '~/components/avatar.vue'
 import SocialButtons from '../components/socialButtons.vue'
 export default {
-  components: { iconButton, SocialButtons },
+  components: { iconButton, SocialButtons, avatar },
   name: 'IndexPage',
   data() {
     return {
+      profile: profile,
       tools: tools,
       langs: langs,
-      social: social,
     }
   },
+  mounted() {},
 }
 </script>
